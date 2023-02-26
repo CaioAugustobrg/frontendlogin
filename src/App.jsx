beda35b53816d567eb5cce63b8372054a271f6ec
Caio Braga <caioaugustobrg@gmail.com>
2023-02-26 02:14:32 -0300

    identation

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
-import Login from './pages/Login'
-import { createGlobalStyle } from "styled-components";
+import { React, useState } from 'react';
+import reactLogo from './assets/react.svg';
+import './App.css';
+import Login from './pages/Login';
+import { createGlobalStyle } from 'styled-components';
 
 
- const GlobalStyle = createGlobalStyle`
+const GlobalStyle = createGlobalStyle`
   
     @import url('https://fonts.googleapis.com/css2?family=Inter:wght@400;600;700&display=swap');
     font-family: 'Inter', sans-serif;
   
-      `
+      `;
 
 function App() {
-  const [count, setCount] = useState(0)
+	const [count, setCount] = useState(0);
 
-  return (
-    <div className="App">
-     <GlobalStyle />
-     <Login /> 
+	return (
+		<div className="App">
+			<GlobalStyle />
+			<Login /> 
      
-    </div>
-  )
+		</div>
+	);
 }
 
-export default App
+export default App;
