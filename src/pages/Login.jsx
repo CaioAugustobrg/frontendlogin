a835130d8341c73c6e82d1b65baa69bff0298140
Caio Braga <caioaugustobrg@gmail.com>
2023-02-26 02:03:32 -0300

    identation

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,8 +1,9 @@
-import { useState } from "react";
-import { Topo, Container, Title, Button, Form } from "../styles";
-import { createGlobalStyle } from "styled-components";
-import styled from "styled-components";
-import axios from "axios";
+import { React ,useState } from 'react';
+import { Topo, Container, Title, Button, Form } from '../styles';
+import { createGlobalStyle } from 'styled-components';
+// eslint-disable-next-line no-unused-vars
+import styled from 'styled-components';
+import axios from 'axios';
 
 const GlobalStyle = createGlobalStyle`
   body {
@@ -11,68 +12,72 @@ const GlobalStyle = createGlobalStyle`
   }`;
 
 function Login() {
-  const [email, setEmail] = useState("")
-  const [password, setPassword] = useState("")
-  
- const onSubmit = async (e) => {
-  e.preventDefault()
-  console.log(email, password)
-  
+	const [email, setEmail] = useState('');
+	const [password, setPassword] = useState('');
 
-  try {
-    await axios.post('http://localhost:3030/login', 
-      JSON.stringify({email, password}),
-      {
-        headers: { 'Content-Type': 'application/json'}
-      }  
-    )
-  } catch (error) {
-    erro: true,
-    console.log(error)
-  }
- }
-  return (
-    <>
-      <GlobalStyle />
-      <Container>
-        <Topo>
-          <Title>
+	const onSubmit = async (e) => {
+		e.preventDefault();
+		console.log(email, password);
+
+		try {
+			await axios.post(
+				'http://localhost:3030/login',
+				JSON.stringify({ email, password }),
+				{
+					headers: { 'Content-Type': 'application/json' },
+				}
+			);
+		} catch (error) {
+			true,
+			console.log(error);
+		}
+	};
+	return (
+		<>
+			<GlobalStyle />
+			<Container>
+				<Topo>
+					<Title>
             Welcome to ng.cash application
-            <span>
-              <p> a young way to take care of your cash!</p>
-            </span>
-          </Title>
-        </Topo>
-        <main>
-            <Form>
-            <form >
-              <label>Write your best email</label>
-              <input
-                type="email"
-                name="email"
-                required
-                placeholder="johnnysilverhand@email.com"
-                onChange={(e) => setEmail(e.target.value)}
-                value={email}
-              ></input>
+						<span>
+							<p> a young way to take care of your cash!</p>
+						</span>
+					</Title>
+				</Topo>
+				<main>
+					<Form>
+						<form>
+							<label>Write your best email</label>
+							<input
+								type="email"
+								name="email"
+								required
+								placeholder="johnnysilverhand@email.com"
+								onChange={(e) => setEmail(e.target.value)}
+								value={email}
+							></input>
 
-              <label>Write your password</label>
-              <input
-                type="password"
-                name="password"
-                required
-                placeholder="********"
-                onChange={(e) => setPassword(e.target.value)}
-                value={password}
-              ></input>
-              <Button><button type="submit" onClick={(e) => onSubmit(e)}>ENTER!</button></Button>
-              <span>Do not have an account?</span>
-            </form>
-          </Form>  
-        </main>
-      </Container>
-    </>
-  );
+							<label>Write your password</label>
+							<input
+								type="password"
+								name="password"
+								required
+								placeholder="********"
+								onChange={(e) => setPassword(e.target.value)}
+								value={password}
+							></input>
+							<Button>
+								<button type="submit" onClick={(e) => onSubmit(e)}>
+                  ENTER!
+								</button>
+							</Button>
+							<span>Do not have an account?</span>
+						</form>
+					</Form>
+				</main>
+			</Container>
+		</>
+	);
 }
 
 export default Login;
