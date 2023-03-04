415ce8fa7347638578f5a90bb21001f43852968f
Caio Braga <caioaugustobrg@gmail.com>
2023-03-04 20:09:53 -0300

    :recycle: refactor: changes in styled-components structure(doom) and design

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,6 +1,6 @@
 import axios from 'axios';
 import React, { useState } from 'react';
-import { Topo, Container, Title, Button, Form, Label, Input } from '../styles/styles.register';
+import { Container, Header, Button, Main, Label, Input } from '../styles/styles.register';
 
 const Register = () => {
 	const [username, setUsername] = useState('');
@@ -30,51 +30,52 @@ const Register = () => {
 		<>
 		
 			<Container>
-				<Topo>
-					<Title>
+				<Header>
+					<h1>
 						Welcome to ng.cash application
-						<span>
-							<p> a young way to take care of your money!</p>
-						</span>
-					</Title>
-				</Topo>
-				<main>
-					<Form method='POST'>
-						<form method='POST' >
-							<Label>Write your username</Label>
-							<Input
-								type='text'
-								name='username'
-								required
-								placeholder='Johnny Silverhand'
-								onChange={(e) => setUsername(e.target.value)}
-								value={username}
-							></Input>
-							<Label>Write your best email</Label>
-							<Input
-								type="email"
-								name="email"
-								required
-								placeholder="johnnysilverhand@email.com"
-								onChange={(e) => setEmail(e.target.value)}
-								value={email}
-							></Input>
+					</h1>
+					<span>
+					a young way to take care of your money!
+					</span>
+				</Header>
 
-							<Label>Write your password</Label>
-							<Input
-								type="password"
-								name="password"
-								required
-								placeholder="********"
-								onChange={(e) => setPassword(e.target.value)}
-								value={password}
-							></Input>
-							<Button type="submit" onClick={(e) => onSubmit(e)}>
+				<Main>
+					<form method='POST'>
+					
+						<Label>Write your username</Label>
+						<Input
+							type='text'
+							name='username'
+							required
+							placeholder='Johnny Silverhand'
+							onChange={(e) => setUsername(e.target.value)}
+							value={username}
+						></Input>
+						<Label>Write your best email</Label>
+						<Input
+							type="email"
+							name="email"
+							required
+							placeholder="johnnysilverhand@email.com"
+							onChange={(e) => setEmail(e.target.value)}
+							value={email}
+						></Input>
+
+						<Label>Write your password</Label>
+						<Input
+							type="password"
+							name="password"
+							required
+							placeholder="********"
+							onChange={(e) => setPassword(e.target.value)}
+							value={password}
+						></Input>
+						<Button type="submit" onClick={(e) => onSubmit(e)}>
 							REGISTER</Button>
 							
-						</form>
-					</Form>
-				</main>
+				
+					</form>
+				</Main>
 			</Container>
 		</>
 	);
