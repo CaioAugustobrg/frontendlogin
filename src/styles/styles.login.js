d1c445fc5a291257247e07f9b6b87d686d3ff986
Caio Braga <caioaugustobrg@gmail.com>
2023-03-04 19:43:28 -0300

    :lipstick: feat: some changes in styled-components structure and design

--- src/styles/styles.login.js
+++ src/styles/styles.login.js
@@ -4,64 +4,74 @@ export const Container = styled.div`
   @import url("https://fonts.googleapis.com/css2?family=Inter:wght@400;600;700&display=swap");
   flex-direction: column;
   background-color: #000;
+  width: 550px;
+  margin: auto;
 `;
 
-export const Topo = styled.header`
+export const Header = styled.header`
 display: flex;
-flex-direction: column`;
+flex-direction: column;
+text-align: center;
+width: 100%;
+
+h1 {
+  color: #f3e600;
+};
+span {
+  color: #9400d3;
+}`;
+
+export const Main = styled.main`
+flex-direction: column;
+display: flex;
+width: 100%;
+`;
 
 export const Form = styled.form`
   @import url("https://fonts.googleapis.com/css2?family=Inter:wght@400;600;700&display=swap");
   background-color: #6271;
   flex-direction: column;
   display: flex;
-  width: 500px;
+  width: 100%;
   height: 300px;
   margin: 0 auto;
- 
-  span {
-    color: #ff003c;
-   h1 {
-    height: 10px;
-   }
-    p {
-      font-family: "Inter", sans-serif;
-      height: 10px;
-    }
-  }
+  gap: 10px;
+  align-items: center;
 `;
 
-export const Title = styled.h1`
+export const Title = styled.div`
+display: flex;
+
+h1 {
   @import url("https://fonts.googleapis.com/css2?family=Inter:wght@400;600;700&display=swap");
   text-align: center;
   color: #f3e600;
   font-weight: bold;
   font-size: 25px;
   height: 10px;
+}
+ 
 `;
 
-export const Subtitle = styled.div`
-display: flex;
-height: 10px;
-span {
-  color: red;
-};
-`;
+
 export const Label = styled.label`
     text-align: left;
     color: #f3e600;
     font-family: "Inter", sans-serif;
     font-size: 20px;
+    width: 100%;
+    margin-top: 20px;
 `;
 
 export const Input = styled.input`
+display: flex;
 background-color:#9400d3;
 color: #000;
 border: none;
 font-size: 15px;
 font-weight: 800;
 width: 100%;
-height: 150px;
+height: 30px;
 border: 1px solid #f3e600;
 
 &:focus {
@@ -72,7 +82,7 @@ border: 1px solid #f3e600;
 export const Button = styled.button`
   margin-top: 15px;
   width: 100%;
-  height: 300px;
+  height: 80px;
   background-color: #f3e600;
   color: #9400d3;
   font-size: 25px;
@@ -82,4 +92,14 @@ export const Button = styled.button`
   &:hover {
     background-color: aqua;
   }
+`;
+
+export const Footer = styled.footer`
+a {
+  color: #9400d3;
+  &:hover {
+    color: aqua;
+  };
+}
+  
 `;
