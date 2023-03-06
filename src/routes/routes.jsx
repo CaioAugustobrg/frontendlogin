745d19122049b44c649095764f60f4c84956e2de
Caio Braga <caioaugustobrg@gmail.com>
2023-03-06 15:08:16 -0300

    :sparkles: feat: route config for new page called Main

--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -4,6 +4,7 @@ as Router,
 Route,
 Routes} from 'react-router-dom';
 import Login from '../pages/Login';
+import Main from '../pages/Main';
 import Register from '../pages/Register';
 
 const Paths = () => { 
@@ -12,6 +13,7 @@ const Paths = () => {
 			<Routes>
 				<Route path="/" element={<Login />} />
 				<Route path="/Register" element={<Register />} />
+				<Route path='/Main' element={<Main />} />
 			
 			</Routes>
 		</Router>
