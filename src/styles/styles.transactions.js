ad0cbbef8d472d11028d2aa36b7e728de6873cf7
Caio Braga <caioaugustobrg@gmail.com>
2023-03-06 22:00:40 -0300

    :lipstick: feat: more style-componets structure and styling

---
+++ src/styles/styles.transactions.js
@@ -0,0 +1,35 @@
+import styled from 'styled-components';
+
+
+export const Container = styled.div`
+  @import url("https://fonts.googleapis.com/css2?family=Inter:wght@400;600;700&display=swap");
+  flex-direction: column;
+  background-color: #000;
+  width: 550px;
+  margin: auto;
+`;
+
+export const Header = styled.header`
+display: flex;
+flex-direction: column;
+width: 100%;
+text-align: center;
+margin: auto;
+
+h1 {
+  color: #f3e600;
+};`;
+
+export const Profile = styled.div`
+background-color: #9400d3;
+display: row;
+
+`;
+
+export const Icon = styled.div`
+background-color: aliceblue;
+`;
+
+export const Main = styled.main`
+width: 100%;
+padding-bottom: 10px;`;
