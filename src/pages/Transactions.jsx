78a0726855b4579472ab6082bc40d654b610d3cf
Caio Braga <caioaugustobrg@gmail.com>
2023-03-21 16:24:29 -0300

    :bug: fix: corrected bugs relationated to cookie

--- src/pages/Transactions.jsx
+++ src/pages/Transactions.jsx
@@ -1,11 +1,46 @@
-import {React, useContext} from 'react';
+import {React, useContext, useEffect} from 'react';
 import {AiOutlineUser} from 'react-icons/ai';
 import userContext from '../context/userContext';
+import { useCookies } from 'react-cookie';
 import { Container, Header, Main, Profile, Icon } from '../styles/styles.transactions';
+
+import apiService from '../services/api';
 
 const Transactions = () => {
 	const {username} = useContext(userContext);
+	const [cookies] = useCookies(['signed_token']);
+	const token = cookies.signed_token;
+	console.log(token);
 	console.log(username);
+
+	async function getAllAccounts() {
+		let accounts;
+		try {
+			accounts = await apiService.get('/getAllAccounts');
+
+		} catch(error) {
+			console.error(error);
+		}
+		
+		console.error(accounts);
+
+	}
+
+	// 	const getAccounts = await axios.get('http://localhost:3030/getAllAccounts',
+	// 		{
+	// 			withCredentials: true,
+	// 		}
+	// 	);
+	// 	console.log(getAccounts);
+	// } catch (error) {
+	// 	console.error(error);
+	// }
+	
+
+	useEffect(() => {
+		getAllAccounts();
+	}, []);
+
 	return (
 		<>
 			<Container>
@@ -19,11 +54,12 @@ const Transactions = () => {
 				</Header>
 				<Main>
 					<Profile>
-						<h1>Olá,  {`${username}`}. <Icon><AiOutlineUser /></Icon></h1>
+						<h1>How its going?,  {`${username}`}. <Icon><AiOutlineUser /></Icon></h1>
 						<span>Your currently balance is: </span>
 					</Profile>
 				</Main>
 			</Container>
 		</>
 	);
-}; export default Transactions;
+}; 
+export default Transactions;
