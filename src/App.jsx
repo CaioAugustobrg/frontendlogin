cdae6fb7b48491924c0549ff03f2b12482055954
Caio Braga <caioaugustobrg@gmail.com>
2023-03-26 20:54:11 -0300

    :recycle: refactor: removed lines

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import { createGlobalStyle } from 'styled-components';
 import Paths from './routes/routes';
 import userContext from '../src/context/userContext'; 
 
-
 export const GlobalStyle = createGlobalStyle`
   html,body {
 		@import url('https://fonts.googleapis.com/css2?family=Inter:wght@400;600;700&display=swap');
@@ -16,23 +15,19 @@ export const GlobalStyle = createGlobalStyle`
 		height: 100%;
 		width: 100%;
 		background-color: #000;
-	
-   
   }
       `;
 
 function App() {
 	const [username, setUsername] = useState('');
+	
 	return (
 		<userContext.Provider value={{username, setUsername}}>
 			<div className="App">
-	
 				<GlobalStyle />
 				<Paths /> 
-	
 			</div>
 		</userContext.Provider>
 	);
 }
-
 export default App;
